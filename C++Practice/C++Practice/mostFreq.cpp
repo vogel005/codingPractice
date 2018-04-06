0c765602a48c0ff992911a50cd350d9025866267
John Vogel <john.r.vogel@gmail.com>
2018-04-05 17:37:47 -0700

    mostFreq and fillArr
    Added function that fills an array.
    
    Added initial function that finds the most frequent number in an array. Need to add check for numbers that tie for most frequent.
    
    added cin.ignore(); before cin.get() that ends the program, to avoid it reading leftover endlines.

--- C++Practice/C++Practice/mostFreq.cpp
+++ C++Practice/C++Practice/mostFreq.cpp
@@ -1,32 +1,58 @@
 /*
 John Vogel
 Start: 3/26/2018
+End: 
 Prompt: Find the most frequent integer in an array
 */
 #include <iostream>
 #include <cstdlib>
 using namespace std;
 
-//int mostFreq(int temp[], int size);
-void displayArr(int temp[], int size);
+void mostFreq(int temp[], int size); //finds most frequent value in an array
+void displayArr(int temp[], int size); //displays contents of array
+void fillArr(int temp[], int size); //fills users array
 
 int main() {
 
-	int array[10] = { 16, 28, 10, 23, 1, 32, 32, 0, 1, 1 };
+	int array[10] = { 16, 28, 10, 23, 1, 32, 32, 0, 1, 1 }; //static array 
 
 	displayArr(array, 10);
-	cout << "Please press Enter. \n";
+
+	mostFreq(array, 10);
+
+	int userArr[10];
+
+	fillArr(userArr, 10);
+	displayArr(userArr, 10);
+	mostFreq(userArr, 10);
+
+	cin.ignore();
+	cout << "Please press Enter to End Program. \n";
 	cin.get();
 
 	return 0;
 }
 
-/*int mostFreq(temp[a]){
+void mostFreq(int temp[], int size){
 
-
-
+	int mostF = 0; //most frequent number
+	int cnt1=0; //counters
+	int cnt2 = 0;
+	for (int i = 0; i < size; i++) {
+		for (int f = 0; f < size; f++) {
+			if (temp[i] = temp[f]) {
+				cnt1++;
+			}
+			if (cnt1 > cnt2) {
+				mostF = temp[i];
+				cnt2 = cnt1;
+			}
+		}
+	}
+	cout << "The most frequent number in the array is:\n" << mostF << "\n";
+	return;
 }
-*/
+
 
 void displayArr(int temp[], int size) {
 
@@ -35,5 +61,16 @@ void displayArr(int temp[], int size) {
 	}
 	cout << "\n";
 
+	return;
+}
+
+void fillArr(int temp[], int size) {
+
+	cout << "Please type " << size << " numbers:\n";
+	for (int i = 0; i < size; i++) {
+		cout << "Array value " << i << ": ";
+		cin >> temp[i];
+	}
+	cout << "\n";
 	return;
 }
