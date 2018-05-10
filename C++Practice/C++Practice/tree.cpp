e3b13bfc5bb9650c96a0e36fd71d184dc42deeab
John Vogel <john.r.vogel@gmail.com>
2018-05-10 14:39:09 -0700

    Tree class added
    Added class for which I will first make a binary search tree in it.

---
+++ C++Practice/C++Practice/tree.cpp
@@ -0,0 +1,19 @@
+/*
+John Vogel
+Start: 5/10/2018
+Last Modified: 5/10/2018
+Prompt: Implement a BST with insert and delete functions
+*/
+#ifndef TREE_CPP
+#define TREE_CPP
+#include "tree.h"
+
+tree::tree()
+{
+}
+
+tree::~tree()
+{
+}
+
+#endif
