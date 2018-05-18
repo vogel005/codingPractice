8791f2796e607278a7b0cc3e7d2bd756e4199ec7
John Vogel <john.r.vogel@gmail.com>
2018-05-18 15:18:12 -0700

    Sort: started
    sorting class created. will begin to implement a bubble sort for an array.

---
+++ C++Practice/C++Practice/sort.cpp
@@ -0,0 +1,20 @@
+/*
+John Vogel
+Start: 5/18/2018
+Last Modified: 5/18/2018
+Prompt: Implement bubble sort
+*/
+#ifndef SORT_CPP
+#define SORT_CPP
+#include "sort.h"
+
+sort::sort()
+{
+}
+
+
+sort::~sort()
+{
+}
+
+#endif 
---
+++ C++Practice/C++Practice/sort.h
@@ -0,0 +1,21 @@
+/*
+John Vogel
+Start: 5/18/2018
+Last Modified: 5/18/2018
+Prompt: Implement bubble sort
+*/
+#ifndef SORT_H
+#define SORT_H
+#include <iostream>
+using namespace std; 
+
+class sort
+{
+public:
+	sort();
+	~sort();
+
+	//bubble sort
+};
+
+#endif
